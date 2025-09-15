bea94a9044e0d5b818cac79d056beebe0c609c44
alsrhsk <minguan1537@gmail.com>
2025-09-15 11:41:30 +0900

    Add files via upload

---
+++ 05_practice_2.ino.ino
@@ -0,0 +1,23 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+  delay(100);
+}
+
+void loop() {
+  while(count<=10)
+  {
+    toggle = toggle_state(toggle);
+    digitalWrite(PIN_LED, toggle);
+    delay(1000);
+    count=count+1;
+  }
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+}
